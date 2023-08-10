bc0ccda314d6356473f7b547a4af958a5ec49173
bono-94 <bono.94@hotmail.com>
2023-08-10 10:29:23 +0000

    Introduce props to the react app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 
 function App() {
   return (
@@ -15,8 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn how to use React
         </a>
+        <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32"/>
       </header>
     </div>
   );
