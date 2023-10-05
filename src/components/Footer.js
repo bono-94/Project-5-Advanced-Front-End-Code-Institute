219b35bff96f58a04aef9c88af602c9c3d36cc99
bono-94 <bono.94@hotmail.com>
2023-10-05 11:14:56 +0000

    Responsively style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,44 +5,47 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import logo from "../assets/logo.png";
 import styles from "../styles/Footer.module.css";
+import { NavLink } from "react-bootstrap";
 
 function Footer() {
   return (
-    <footer>
-      <Container className={styles.Footer} fluid>
-        <Row className={styles.Footer} fluid>
-          <Col md={4}>
-            <img
-              src={logo}
-              alt="Brand Logo"
-              width="100"
-              className={styles.Spinner}
-            />
-            <h5>Brand Name</h5>
+    <footer className={styles.Footer}>
+      <Container fluid>
+      <Row fluid>
+  <Col className={`d-flex align-items-center justify-content-between ${styles.LogoButtonContainer}`}>
+    <img
+      src={logo}
+      alt="Brand Logo"
+      width="100"
+      className={styles.Spinner}
+    />
+    <Button
+      variant="link"
+      className={`text-light align-self-start`}
+      onClick={() => window.scrollTo(0, 0)}
+    >
+      <i className="fas fa-arrow-circle-up"></i>
+    </Button>
+  </Col>
+</Row>
+      <Row fluid>
+       <Col className={`${styles.Contact}`}>
+            <h5>KNOWL3DG3 CONTAINERS</h5>
             <p>
-              <i className="fas fa-map-marker-alt"></i> Address: 123 Main St,
-              City, Country
+              <i className="fas fa-map-marker-alt"></i> 123 Main St, New York, USA
             </p>
             <p>
-              <i className="fas fa-phone"></i> Contact: +1 (123) 456-7890
+              <i className="fas fa-phone"></i> +1 (123) 456-7890
             </p>
             <p>
-              <i className="fas fa-envelope"></i> Email: info@example.com
+              <i className="fas fa-envelope"></i> knowl3dg3@containers.com
             </p>
-          </Col>
-          <Col md={8}>
-            <Button
-              variant="link"
-              className="text-light float-right"
-              onClick={() => window.scrollTo(0, 0)}
-            >
-              <i className="fas fa-arrow-circle-up fa-2x"></i>
-            </Button>
           </Col>
         </Row>
+        
         <hr className="bg-light" />
         <Row>
-          <Col>
+          <Col className={`${styles.Social}`}>
             <div className="d-flex justify-content-center justify-content-md-end">
               <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-facebook fa-2x mr-3"></i>
