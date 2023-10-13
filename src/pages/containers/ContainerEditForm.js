e9df7fb991f59c4b7c152508b40bc2238f084762
bono-94 <bono.94@hotmail.com>
2023-10-13 04:37:26 +0000

    Style page that edits containers

--- src/pages/containers/ContainerEditForm.js
+++ src/pages/containers/ContainerEditForm.js
@@ -2,10 +2,12 @@ import React, { useEffect, useRef, useState } from "react";
 import { useHistory, useParams } from "react-router";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
 import { axiosReq } from "../../api/axiosDefaults";
-import styles from "../../styles/ContainerEditForm.module.css";
+import styles from "../../styles/ContainerCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
@@ -78,68 +80,70 @@ function ContainerEditForm() {
     }
   };
 
+  const allFields = (
+    <div className="text-center">
+      <Form.Group>
+      <Form.Label>Public Visibility</Form.Label>
+        <Form.Check
+          type="switch"
+          id="custom-switch"
+          name="is_public"
+          checked={containerData.is_public}
+          onChange={(e) => setContainerData({ ...containerData, is_public: e.target.checked })}
+          className={styles.Checkbox}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Container Name</Form.Label>
+        <Form.Control
+          type="text"
+          name="container_name"
+          value={container_name}
+          onChange={handleChange}
+          className={styles.Input}
+          placeholder="Name your container..."
+        />
+      </Form.Group>
+      {errors?.container_name?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Form.Group>
+        <Form.Label>Container Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={6}
+          name="container_info"
+          value={container_info}
+          onChange={handleChange}
+          className={styles.Input}
+          placeholder="About container..."
+        />
+      </Form.Group>
+      {errors?.container_info?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
+        Create
+      </Button>
+    </div>
+  );
+
   return (
-    <Form onSubmit={handleSubmit}>
-      <Container className={`${appStyles.Content} ${styles.Container}`}>
-        {/* Add form fields for container editing */}
-        <Form.Group>
-  <Form.Check
-    type="checkbox"
-    label="Public"
-    name="is_public"
-    checked={is_public}
-    onChange={(e) =>
-      setContainerData({
-        ...containerData,
-        is_public: e.target.checked,
-      })
-    }
-  />
-  </Form.Group>
-    <Form.Group>
-    <Form.Label>Container Name</Form.Label>
-    <Form.Control
-        type="text"
-        name="container_name"
-        value={container_name}
-        onChange={handleChange}
-    />
-    </Form.Group>
-    {errors?.container_name?.map((message, idx) => (
-    <Alert variant="warning" key={idx}>
-        {message}
-    </Alert>
-    ))}
-
-    <Form.Group>
-    <Form.Label>Container Description</Form.Label>
-    <Form.Control
-        as="textarea"
-        rows={6}
-        name="container_info"
-        value={container_info}
-        onChange={handleChange}
-    />
-    </Form.Group>
-    {errors?.container_info?.map((message, idx) => (
-    <Alert variant="warning" key={idx}>
-        {message}
-    </Alert>
-    ))}
-
-        {/* Add other form fields for container editing here */}
-
-        <Button
-          className={`${btnStyles.Button} ${btnStyles.Blue}`}
-          onClick={() => history.goBack()}
-        >
-          Cancel
-        </Button>
-        <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-          Save
-        </Button>
-      </Container>
-    </Form>
+    <Row className={styles.Row}>
+      <Col className={` py-2 p-md-2 ${styles.SupportCol}`}>
+        <Container  className={`${appStyles.Content} p-4 ${styles.Container}`}>
+        <h1 className={styles.Header}>Edit Container</h1>
+          <Form onSubmit={handleSubmit}>
+            {allFields}
+          </Form>
+        </Container>
+      </Col>
+    </Row>
   );
 }
 
